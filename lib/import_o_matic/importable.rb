d9b5cbc3fde263ef767b450505a48c47807ac454
Ruben Sierra <maguilag@gmail.com>
2013-11-11 15:43:28 +0100

    Import log.

--- lib/import_o_matic/importable.rb
+++ lib/import_o_matic/importable.rb
@@ -1,6 +1,7 @@
 # import_o_matic/lib/import_o_matic/importable.rb
 require 'import_o_matic/formats/csv'
 require 'import_o_matic/options'
+require 'import_o_matic/logger'
 
 module ImportOMmatic
   module Importable
@@ -11,12 +12,15 @@ module ImportOMmatic
 
     module ClassMethods
       def import_o_matic import_class = ImportOMmatic::Options
-        cattr_accessor :import_options
+        cattr_accessor :import_options, :import_log
         self.import_options = import_class.new self
       end
 
       def import_from_file file_path
+        self.import_log = ImportOMmatic::Logger.new(self.name.downcase)
+        self.import_log.info "---- Init #{self.name.downcase} importation from file #{file_path}"
         format_class = "import_o_matic/formats/#{import_options.format.to_s}".classify.constantize
+
         format_class.import_from_file file_path, import_options.format_options do |row|
           attributes = {}
           import_options.columns.each do |column, attribute|
@@ -27,19 +31,25 @@ module ImportOMmatic
           end
           action = row[import_options.incremental_action_column.to_s]
           incremental_id = row[import_options.incremental_id_column.to_s]
-          self.import_attributes attributes, action, incremental_id
+          self.import_log.counter :total
+          item = self.import_attributes attributes, action, incremental_id
+          self.import_log.print_errors(row, item) if item.errors.any?
         end
+        self.import_log.finish
       end
 
       def import_attributes attributes, action, incremental_id
         case action
         when import_options.actions[:update]
+          self.import_log.counter import_options.actions[:update]
           element = self.where(import_options.incremental_id_attribute => incremental_id).first
           element.update_attributes attributes if element
         when import_options.actions[:destroy]
+          self.import_log.counter import_options.actions[:destroy]
           element = self.where(import_options.incremental_id_attribute => incremental_id).first
           element.destroy if element
         else
+          self.import_log.counter import_options.actions[:create]
           self.create attributes
         end
       end
---
+++ lib/import_o_matic/logger.rb
@@ -0,0 +1,39 @@
+module ImportOMmatic
+
+  class Logger < ::Logger
+    attr_accessor :counters
+
+    def format_message(severity, timestamp, progname, msg)
+      "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+    end
+
+    def initialize object_name
+      self.counters = {}
+      import_path = "log/importations/#{object_name}"
+      FileUtils.mkdir_p(import_path) unless File.directory?(import_path)
+      timestamp = Time.now.utc.iso8601.gsub(/\W/, '')
+      file_name = "#{timestamp}_#{object_name}_import.log"
+      super "#{import_path}/#{file_name}"
+    end
+
+    def finish
+      self.info "---- End of importation"
+      self.info "---- Results:"
+      self.counters.each do |key, value|
+        self.info "\t\t#{key.upcase}: #{value}" if key
+      end
+    end
+
+    def counter kind
+      counters[kind] = counters[kind].to_i.next
+    end
+
+    def print_errors row, item
+      self.counter :errors
+      self.error "-- Errors with row:"
+      self.error "\t#{row.to_s.strip}"
+      self.error "\t#{item.errors.full_messages.to_sentence}"
+    end
+  end
+
+end
