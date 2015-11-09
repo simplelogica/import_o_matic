4455d48c612ccf56396a4977730207356722310a
Ruben Sierra <maguilag@gmail.com>
2015-11-09 19:18:26 +0100

    Add old logs clear method

--- lib/import_o_matic/logger.rb
+++ lib/import_o_matic/logger.rb
@@ -7,11 +7,13 @@ module ImportOMmatic
       "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
     end
 
-    def initialize object_name
+    def initialize object_name, max_logs = 10
       object_name.gsub!('/', '_')
       self.counters = {}
       log_dir = "log/importations/#{object_name}"
       FileUtils.mkdir_p(log_dir) unless File.directory?(log_dir)
+      # Remove all logs unless max_logs -1 and create a new one
+      clear_logs log_dir, max_logs.pred
       timestamp = Time.now.utc.iso8601.gsub(/\W/, '')
       file_name = "#{timestamp}_#{object_name}_import.log"
       super self.path = "#{log_dir}/#{file_name}"
@@ -34,6 +36,14 @@ module ImportOMmatic
       self.error "-- Errors with data:"
       self.error "\t#{text}"
       self.error "\t#{item.errors.full_messages.to_sentence}"
+    end
+
+    protected
+
+    def clear_logs dir, max_logs
+      old_logs = Dir.glob("#{dir}/*").sort_by { |path| File.mtime(path) }.reverse
+      old_logs.shift(max_logs) if max_logs > 0
+      File.delete(*old_logs)
     end
   end
 
